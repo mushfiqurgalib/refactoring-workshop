f9daaaa4be11195c07cb84db7fd26c69580f3e28
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-03-03 09:58:32 +0600

    Create ScienceQuestion.java

---
+++ java/src/main/java/workshop/ScienceQuestion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop;
+
+/**
+ *
+ * @author user
+ */
+public class ScienceQuestion extends Question {
+     public ScienceQuestion() {
+        super();
+    }
+    public String createQuestion(int index)
+    {
+    return "Science question "+ index;
+    }
+    
+}
