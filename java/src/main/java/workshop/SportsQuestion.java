61116142a38d1fe0548c217598501690d211d627
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-03-03 09:57:45 +0600

    Add files via upload

---
+++ java/src/main/java/workshop/SportsQuestion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop;
+
+/**
+ *
+ * @author user
+ */
+public class SportsQuestion extends Question{
+     public SportsQuestion() {
+        super();
+    }
+    public String createQuestion(int index)
+    {
+    return "Sports question "+ index;
+    }
+}
