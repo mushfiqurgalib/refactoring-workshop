eef4bfe82d4af6058fe97f8b8e5787059113d4e5
mushfiqurgalib <48786723+mushfiqurgalib@users.noreply.github.com>
2022-03-03 12:57:31 +0600

    Update PlaintextToHtmlConverter.java

--- java/src/main/java/workshop/PlaintextToHtmlConverter.java
+++ java/src/main/java/workshop/PlaintextToHtmlConverter.java
@@ -8,46 +8,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlaintextToHtmlConverter {
-  
     public String toHtml() throws Exception {
         String text = read();
         String htmlLines = basicHtmlEncode(text);
         return htmlLines;
     }
 
-    private String read() throws IOException {
-    	Path filePath = Paths.get("sample.txt");
-    	byte[] fileByteArray = Files.readAllBytes(filePath);
+    protected String read() throws IOException {
+        Path filePath = Paths.get("sample.txt");
+        byte[] fileByteArray = Files.readAllBytes(filePath);
         return new String(fileByteArray);
     }
 
     private String basicHtmlEncode(String source) {
-        
-       
         List<String> result = new ArrayList<>();
         List<String> convertedLine = new ArrayList<>();
-        
- String characterToConvert = stashNextCharacterAndAdvanceThePointer(source);
-        convertedLine.add(conversion(characterToConvert));
-        addANewLine(result, convertedLine);
-        String finalResult = String.join("<br />", result);
-        return finalResult;
+        for (char characterToConvert : source.toCharArray()) {
+            switch (characterToConvert) {
+                case '<':
+                    convertedLine.add("&lt;");
+                    break;
+                case '>':
+                    convertedLine.add("&gt;");
+                    break;
+                case '&':
+                    convertedLine.add("&amp;");
+                    break;
+                case '\n':
+                    addANewLine(result,convertedLine);
+                    convertedLine = new ArrayList<>();
+                    break;
+                default:
+                    pushACharacterToTheOutput(convertedLine,String.valueOf(characterToConvert));
+            }
+        }
+        addANewLine(result,convertedLine);
+        return String.join("<br />", result);
     }
- private String stashNextCharacterAndAdvanceThePointer(String source) {
-        return null;
-    }
-   
+
+
     //stringfy convertedLine array and push into result
     //reset convertedLine
     private void addANewLine(List<String> result,List<String> convertedLine) {
         String line = String.join("", convertedLine);
         result.add(line);
-        convertedLine = new ArrayList<>();
     }
 
-   
-    
-      private String conversion(String character) {
-        return character == "<" ? "&lt:" : character == ">" ? "&gt;" : character == "&" ? "&amp;" : character;
+    private void pushACharacterToTheOutput(List<String> convertedLine,String characterToConvert) {
+        convertedLine.add(characterToConvert);
     }
 }
